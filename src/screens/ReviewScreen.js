f835e12fd813c026e8057408cea6d2dd8054c376
David Henley <henleydr@gmail.com>
2017-05-01 11:32:54 -0500

    Changed RN back to beta 7

--- src/screens/ReviewScreen.js
+++ src/screens/ReviewScreen.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
+  static navigationOptions = {
+    title: 'Review Screen',
+    header: () => ({
+      right: <Button title="Settings" onPress={() => {}} />
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
